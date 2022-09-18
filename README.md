314c7058a15ce952c7126917f6fcee8c6e2f7a7c
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2022-09-17 23:21:34 -0300

    Creación del proyecto

---
+++ README.md
@@ -0,0 +1,5 @@
+# Proyecto "Manos a la Obra"
+
+## Idea
+
+> Generar un portal donde el movimiento "Manos a la Obra pueda comunicar sus proyectos e ideales.
---
+++ src/js/scripts.js
@@ -0,0 +1,67 @@
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+
+});
+
+const form = document.getElementById('quiero_ayudar');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = form['name']
+    const email = form['email']
+    const phone = form['phone']
+    const description = form['description']
+
+    const button = document.getElementById('submitButton')
+    console.log("Probando");
+});
